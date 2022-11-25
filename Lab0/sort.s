5865cbf0f757572390bf9c9d565d8a9fef8c5720
Bivol Mihai <87128416+MihaiB-dev@users.noreply.github.com>
2022-11-25 17:34:48 +0200

    Read from the terminal and print the answer

--- Lab0/sort.s
+++ Lab0/sort.s
@@ -1,22 +1,50 @@
-# sa se ordoneze crescator sirul
+;# sa se ordoneze crescator sirul
 .data
-sir: .long 14, 2, 8, 9, 13
-n: .long 5
-
-j: .long 0
+n: .space 4
+a: .space 4
+sir: .space 800
+cit_un_numar: .asciz "%ld"
+print: .asciz "%ld "
+endline: .asciz "\n"
 .text
 
 .global main
 main:
 ;#
+pushl $n
+pushl $cit_un_numar
+call scanf
+popl %ebx
+popl %ebx
+
+lea sir, %esi
 xorl %ecx, %ecx
+for_citire:
+    cmp n, %ecx
+    je exit_for_citire
+
+    pusha
+    pushl $a
+    pushl $cit_un_numar
+    call scanf
+    popl %ebx
+    popl %ebx
+    popa
+
+    movl a, %eax
+    movl %eax, (%esi, %ecx, 4)
+    incl %ecx
+    jmp for_citire
+exit_for_citire:
+
+
+xorl %ecx, %ecx
+lea sir, %esi
 loop:
       ;# ecx este 0, 1, 2, 3, 4
 
     cmp n, %ecx
-    je exit
-
-    lea sir, %esi
+    je end_loop
 
     movl %ecx, %ebx
     loop_j:
@@ -40,10 +68,31 @@ loop:
 
 incl %ecx
 jmp loop
+end_loop:
 
+lea sir, %esi
+xorl %ecx, %ecx
+for_print:
+    cmp n, %ecx
+    je exit
 
+    movl (%esi, %ecx,4), %ebx
+
+    pusha
+    pushl %ebx
+    pushl $print
+    call printf
+    popl %eax
+    popl %eax
+    popa
+
+    incl %ecx
+jmp for_print
 
 exit:
+pushl $endline
+call printf
+popl %ebx
 movl $1, %eax
 xorl %ebx, %ebx
 int $0x80
