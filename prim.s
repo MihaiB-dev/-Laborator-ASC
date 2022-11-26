6c05d3c8a5f5432a58596166e0eb4a717d7f359e
Bivol Mihai <87128416+MihaiB-dev@users.noreply.github.com>
2022-11-26 18:47:26 +0200

    Adaugare explicatii

--- prim.s
+++ prim.s
@@ -7,36 +7,40 @@ cor: .asciz "Numarul este prim\n"
 .global main
 main:
 
+;# citim numarul de la tastatura
 pushl $n
 pushl $cit
 call scanf
 popl %ebx
 popl %ebx
 
-movl $2, %ecx
-for:
+movl $2, %ecx ;
+for: ;# for(ecx = 2, ecx < n, ecx ++)
     cmp n, %ecx
-    je corect
+    je corect #daca nu iese din loop e corect
 
     movl n, %eax
     xorl %edx,%edx
     div %ecx
+    
     cmp $0, %edx
-    je gresit
-
+    je gresit 
+    ;#daca %eax/%ecx are restul 0 atunci NU e prim
+    
     incl %ecx
     jmp for
 
 corect:
-pushl $cor
+pushl $cor ;# "Numarul este prim\n"
 call printf
 popl %ebx
 jmp exit
 
 gresit:
-pushl $gre
+pushl $gre ;# "Numarul nu este prim\n"
 call printf
 popl %ebx
+
 exit:
 movl $1, %eax
 xorl %ebx, %ebx
